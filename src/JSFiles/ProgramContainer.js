b595c53207d12f362fd7c10375cb5635d9c095dc
AkashLM <malekarakash70@gmail.com>
2022-02-25 12:11:56 +0530

    Layout for program container completed

--- src/JSFiles/ProgramContainer.js
+++ src/JSFiles/ProgramContainer.js
@@ -3,10 +3,41 @@ import '../CSSFiles/ProgramContainer.css';
 const ProgramContainer = () =>{
     return(
         <div className='Main-container'>
+            {/* Container for heading */}
             <div className='Main-container-Top'>
+                <div className='Main-container-Top-left'>
+                    <div className='Share'>
+                        Share
+                    </div>
+                </div>
+
+                <div className='Main-container-Top-right'>
+                    <div className='Main-container-Top-right-Section-A'>
+                        Like
+                    </div>
+                    <div className='Main-container-Top-right-Section-B'>
+                        Stream
+                    </div>
+
+                </div>
+
             </div>
 
+            {/* Container for Buttons */}
             <div className='Main-container-Bottom'>
+                <div className='LogIn-Section'>
+                    <div className='LogIn-Button'>
+                        Login
+                    </div>
+
+                </div>
+
+                <div className='SignIn-Section'>
+                    <div className='SignIn-Button'>
+                        Sign in
+                    </div>
+
+                </div>
             </div>
 
         </div>
